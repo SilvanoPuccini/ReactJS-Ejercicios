2ac02183dadb1b6b7ce99a96e52eb0c24a59b347
Grupo2 <grupo2@test.com>
2026-02-08 18:07:54 -0300

    configuracion de git page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  base: "/ReactJS-Ejercicios/",
+  base: "https://silvanopuccini.github.io/ReactJS-Ejercicios/",
   plugins: [react()],
   build: {
     rollupOptions: {
